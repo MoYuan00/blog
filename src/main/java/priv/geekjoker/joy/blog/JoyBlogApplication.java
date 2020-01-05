15225890b8362dbe25f3c6f2e11fe15f764f2bbe
geekjoker-joy <geekjoker@foxmail.com>
2020-01-05 22:38:34 +0800

    使用maven构建出spring boot项目，并且可以运行成功。

---
+++ src/main/java/priv/geekjoker/joy/blog/JoyBlogApplication.java
@@ -0,0 +1,16 @@
+package priv.geekjoker.joy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author -ZQ-
+ */
+@SpringBootApplication
+public class JoyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/geekjoker/joy/blog/JoyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.geekjoker.joy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
